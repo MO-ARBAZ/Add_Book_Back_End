63ec0699cc1e0bb43fa8324dc2ec8306a593b9ed
MO-ARBAZ <105699855+MO-ARBAZ@users.noreply.github.com>
2024-11-14 18:09:48 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/bookDB", {
+    await mongoose.connect("mongodb+srv://arbaj:2V6_JkBxk_znUvz@cluster0.6qkib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
       //   useNewUrlParser: true,
       //   useUnifiedTopology: true,
     });
